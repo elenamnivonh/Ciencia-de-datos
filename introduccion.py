ce3c4954d892f93e3ff01e04b7eaad429603bbe6
elenamnivonh <a01174666@tec.mx>
2023-11-01 16:58:19 -0600

    Creación de rama fronend

--- introduccion.py
+++ introduccion.py
@@ -1,4 +1,4 @@
-print("Hola, mundo, esta es mi primer interfaz.")
+print("Hola, mundo, esta es mi primer interfaz, Disfrutala")
 
 a = 50
 b = 56
@@ -8,3 +8,7 @@ def suma():
     print(a+b+c)
 
 suma()
+
+def mostar():
+    print("Esta rama está destinada para los cambions necesarios en el fronend")
+mostar()
